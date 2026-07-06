30b266c0d6b47f1639f83cc8ff3d67259b2a99c7
Priya B.G <priyabg777@gmail.com>
2026-07-06 19:31:00 +0530

    Add files via upload

---
+++ audio_processor.py
@@ -0,0 +1,26 @@
+import yt_dlp
+from pydub import AudioSegment
+import os
+
+DOWNLOAD_DIR = 'downloades'
+os.makedirs(DOWNLOAD_DIR,exist_ok = True)
+
+def download_youtube_audio(url : str) ->str:
+    output_path = os.path.join(DOWNLOAD_DIR, "%(title)s.%(ext)s")
+    ydl_opts = {
+        "format": "bestaudio/best",
+        "outtmpl": output_path,  
+        "quiet": True,
+        "extractor_ags": {
+            "youtube": {
+                "player_client":
+        ["android"]
+            }
+        }
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=True)
+        filename = ydl.prepare_filename(info).replace(".webm", ".wav").replace(".m4a", ".wav")
+    return filename
+
+print(download_youtube_audio("https://www.youtube.com/watch?v=dQw4w9WgXcQ"))
